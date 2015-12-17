a45113b92ccc3200d36775dcadd49ff3ff9710b6
pcroce <pscroce@gmail.com>
2015-12-16 22:21:40 -0500

    Add Recipes with ingredients and directions and nested form

--- Gemfile
+++ Gemfile
@@ -11,7 +11,10 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'haml', '~> 4.0', '>= 4.0.7'
 gem 'simple_form', '~> 3.2'
+gem 'paperclip', '~> 4.3', '>= 4.3.2'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
+gem 'cocoon', '~> 1.2', '>= 1.2.6'
+gem 'devise', '~> 3.5', '>= 3.5.2'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -41,7 +41,9 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description)
+    params.require(:recipe).permit(:title, :description, :image,
+    ingredients_attributes: [:id, :name, :_destroy],
+    directions_attributes: [:id, :step, :_destroy])
   end
 
   def find_recipe
